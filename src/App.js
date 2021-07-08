95615ce143728cdd30474496b1b4a9c17e544db3
maopop <sadekmar1@gmail.com>
2021-07-08 08:15:08 -0700

    Project commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import './App.scss';
 import React from 'react';
-
 import RouterComponent from './routing/RouterComponent.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/shared/Header';
